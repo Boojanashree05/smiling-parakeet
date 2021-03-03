6067077b78289ea4179b3aa407b4c40b81dcf179
Boojanashree05 <79972173+Boojanashree05@users.noreply.github.com>
2021-03-03 15:36:02 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Form Page </title>
+  </head>
+  <body>
+    <form>
+      Firstname:
+      <input type="text"placeholder="First_name"name="firstname">
+      <br>
+      <br>
+      Lastname:
+      <input type="text"placeholder="last_name"name="lastname">
+      <br>
+      <br>
+      <input type ="submit"name="submit">
+    </form>
+  </body>
+</html>
